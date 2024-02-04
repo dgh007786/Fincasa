affe7edc47d502815893525c9809f9474f7eeb3f
gunjan dayani <41314977+dgh007786@users.noreply.github.com>
2024-02-04 13:21:57 -0500

    Update next.config.js

--- fincasa-webdev/next.config.js
+++ fincasa-webdev/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Use the basePath and assetPrefix settings if your site is not at the root of the domain.
-  basePath: '/Fincasa/fincasa-webdev/', // Replace 'repository-name' with the name of your repository.
-  assetPrefix: '/Fincasa/fincasa-webdev/',
+  basePath: '/Fincasa/fincasa-webdev', // Replace 'repository-name' with the name of your repository.
+  assetPrefix: '/Fincasa/fincasa-webdev',
 
   // Disable server-side image optimization since it's not supported by static export
   images: {
